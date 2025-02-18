1d55ae5e2b066d1a53f664e540fd9347406dc6b9
Drew Bollinger <drew.bollinger@gsa.gov>
2025-02-18 12:59:21 -0500

    update auth to match editor, other small improvements

--- .profile
+++ .profile
@@ -1,3 +1,4 @@
+export PAYLOAD_SECRET="$(echo "$VCAP_SERVICES" | jq --raw-output --arg service_name "pages-editor-creds" ".[][] | select(.name == \$service_name) | .credentials.PAYLOAD_SECRET")"
 export PAYLOAD_API_KEY="$(echo "$VCAP_SERVICES" | jq --raw-output --arg service_name "pages-editor-creds" ".[][] | select(.name == \$service_name) | .credentials.PAYLOAD_API_KEY")"
 export OAUTH_CLIENT_ID="$(echo "$VCAP_SERVICES" | jq --raw-output --arg service_name "pages-editor-creds" ".[][] | select(.name == \$service_name) | .credentials.OAUTH_CLIENT_ID")"
 export OAUTH_CLIENT_SECRET="$(echo "$VCAP_SERVICES" | jq --raw-output --arg service_name "pages-editor-creds" ".[][] | select(.name == \$service_name) | .credentials.OAUTH_CLIENT_SECRET")"
--- oauth/index.js
+++ oauth/index.js
@@ -2,40 +2,57 @@ const path = require('node:path')
 const bodyParser = require('body-parser');
 const express = require('express');
 const cookieParser = require('cookie-parser')
-const { decodeJwt, createRemoteJWKSet, jwtVerify } = require("jose");
+const crypto = require("node:crypto");
+const { jwtVerify} = require("jose");
 
 const app = express();
+
+const UNAUTHORIZED = 'You are not authorized to visit this site.'
 
 app.use(cookieParser())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(async (req, res, next) => {
-    const token = req.cookies['payload-token']
-    // TODO: how do we remove this, local cannot do claim verification
-    // check for non-expiration, make this better
-    let user = {}
-    if (!process.env.OAUTH_JWT_SET || (process.env.OAUTH_JWT_SET || '').includes('localhost')) {
-        user = decodeJwt(token);
-        if (user.email) {
-            next()
-        } else {
-            throw new Error("not logged in");
-        }
-    } else {
-        const JWKS = createRemoteJWKSet(new URL(process.env.OAUTH_JWT_SET || ''))
-        const { payload } = await jwtVerify(token, JWKS)
-        user = payload;
-        if (user.sub) {
-            next()
-        } else {
-            throw new Error("not logged in");
-        }
-    }
+    // auth logic from editor app
+    // https://github.com/WilsonLe/payload-oauth2/blob/main/src/auth-strategy.ts
+    const token = req.cookies['payload-token'];
+    const uauth = () => res.status(403).send({ message: UNAUTHORIZED });
+    if (!token) return uauth();
 
+      let jwtUser = null;
+      try {
+        const secret = crypto
+          .createHash("sha256")
+          .update(process.env.PAYLOAD_SECRET)
+          .digest("hex")
+          .slice(0, 32);
+
+        const { payload: verifiedPayload } = await jwtVerify(
+          token,
+          new TextEncoder().encode(secret),
+          { algorithms: ["HS256"] },
+        );
+        jwtUser = verifiedPayload;
+      } catch (e) {
+        // Handle token expiration
+        if (e.code === "ERR_JWT_EXPIRED" || e.code === "ERR_JWS_SIGNATURE_VERIFICATION_FAILED") {
+          return uauth();
+        }
+        console.error(e)
+        return uauth();
+      }
+      if (!jwtUser) return uauth();
+
+      // check site access
+      if (jwtUser.sites.find(s => s.site === 'test')) {
+        next()
+      } else {
+        console.log('wrong site')
+        return uauth();
+      }
 })
 
 app.use(async function(req, res) {
-  console.log(req.path)
   const data = await fetch(path.join(process.env.ASTRO_ENDPOINT, req.path))
   text = await data.text()
   res.set(Object.fromEntries(data.headers))
