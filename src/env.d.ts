8124b6c0a8ba7226baba3f63cc85a9daf51ccdb5
Kevin Masters <kevin.masters@gsa.gov>
2025-09-12 09:19:32 -0400

    chore: move repeated functions and types to common export files

---
+++ src/env.d.ts
@@ -0,0 +1,31 @@
+export interface MediaValueProps {
+  id: number;
+  altText: string;
+  caption: string | null;
+  _status: "published";
+  reviewReady: boolean;
+  prefix: string;
+  updatedAt: string;
+  createdAt: string;
+  url: string;
+  thumbnailURL: string;
+  filename: string;
+  mimeType: string;
+  filesize: number;
+  width: number;
+  height: number;
+  focalX: number;
+  focalY: number;
+  site: {
+    bucket: string;
+  };
+  sizes: {
+    thumbnail: Record<string, any>;
+    square: Record<string, any>;
+    small: Record<string, any>;
+    medium: Record<string, any>;
+    large: Record<string, any>;
+    xlarge: Record<string, any>;
+    og: Record<string, any>;
+  };
+}
