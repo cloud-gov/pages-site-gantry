dcb8c9ba3497f476df692004d96a9123c620bef1
Kevin Masters <kevin.masters@gsa.gov>
2025-10-27 14:22:56 -0400

    chore: refine site search UI
    chore: use next_offset and make limit into a constant variable
    
    chore: tidy up params gathering
    
    chore: refactor paging into a utility, add a state handler for paging offset
    
    fix: next and previous pagination links
    
    fix: minimum pages for pagination error
    
    fix: attempt to fix path to svg sprite
    
    fix: image path for sprite
    
    fix: using uswds next & prev svgs
    
    fix: remove unused parameters
    
    chore: update test to include paging and results count features
    
    fix: use tablet column class for mobile

--- src/utilities/pagination.ts
+++ src/utilities/pagination.ts
@@ -1,3 +1,5 @@
+import navigate_next from "@uswds-images/usa-icons/navigate_next.svg";
+import navigate_before from "@uswds-images/usa-icons/navigate_before.svg";
 export function paginate<T>(items: T[], currentPage: number, pageSize: number) {
   const totalPages = Math.ceil(items.length / pageSize);
   const paginatedItems = items.slice(
@@ -9,4 +11,110 @@ export function paginate<T>(items: T[], currentPage: number, pageSize: number) {
     totalPages,
     paginatedItems,
   };
+}
+
+export function searchPagination(
+  totalPages: number,
+  currentPage: number,
+  limit: number,
+) {
+  function getPageNumbers(totalPages: number, currentPage: number) {
+    const delta = 1;
+    const range = [];
+    const rangeWithDots = [];
+    let lastSeenPage;
+
+    for (let i = 1; i <= totalPages; i++) {
+      if (
+        i === 1 ||
+        i === totalPages ||
+        (i >= currentPage - delta && i <= currentPage + delta)
+      ) {
+        range.push(i);
+      }
+    }
+
+    for (let i of range) {
+      if (lastSeenPage) {
+        if (i - lastSeenPage === 2) {
+          rangeWithDots.push(lastSeenPage + 1);
+        } else if (i - lastSeenPage > 2) {
+          rangeWithDots.push("...");
+        }
+      }
+      rangeWithDots.push(i);
+      lastSeenPage = i;
+    }
+
+    return rangeWithDots;
+  }
+
+  const pages = getPageNumbers(totalPages, currentPage);
+
+  const pagination = `
+    <nav aria-label="Pagination" class="usa-pagination">
+      <ul class="usa-pagination__list">
+        ${
+          currentPage > 1
+            ? `<li class="usa-pagination__item usa-pagination__arrow">
+              
+              <a 
+                href="#"
+                data-page="${(currentPage - 2) * limit + 1}"
+              class="page-number usa-pagination__link usa-pagination__previous-page"
+        aria-label="Previous page">
+                <svg class="usa-icon" aria-hidden="true" role="img">
+                  <use href="${navigate_before.src}" width="${navigate_before.width}" />
+                </svg>
+                <span class="usa-pagination__link-text"> Previous</span>
+              </a>
+            </li>`
+            : ""
+        }
+        ${pages
+          .map((p) =>
+            p === "..."
+              ? `<li
+                  class="usa-pagination__item usa-pagination__overflow"
+                  aria-label="ellipsis indicating non-visible pages"
+                >
+                  <span>…</span>
+                </li>
+              `
+              : `<li class="usa-pagination__item usa-pagination__page-no">
+                <a
+                  class="${`page-number usa-pagination__button${p === currentPage ? " usa-current" : ""}`}"
+                  href="#"
+                  data-page="${p === 1 ? "" : Number((p - 1) * limit + 1)}"
+                  aria-current=""
+                  aria-label="${`Page ${p}`}"
+                >
+                  ${p}
+                </a>
+              </li>`,
+          )
+          .join("")}
+
+        ${
+          currentPage < totalPages
+            ? `<li class="usa-pagination__item usa-pagination__arrow">
+              <a
+                class="page-number usa-pagination__link usa-pagination__next-page"
+                aria-label="Next page"
+                href="#"
+                data-page="${currentPage * limit + 1}"
+              >
+                <span class="usa-pagination__link-text"">Next </span>
+                <svg class="usa-icon" aria-hidden="true" role="img" width="${navigate_next.width}">
+                  <use href="${navigate_next.src}" />
+                </svg>
+              </a>
+            </li>`
+            : ""
+        }
+      </ul>
+    </nav>
+  `;
+
+  return pagination;
 }
